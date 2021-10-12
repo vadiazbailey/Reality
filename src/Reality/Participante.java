391c047b6ffffe9531c96ef09e588e8cff08c2b0
Ayelen Diaz Bailey <vdiazbailey@alumnos.exa.unicen>
2021-10-12 15:54:48 -0300

    iniciamos con la clase participante

---
+++ src/Reality/Participante.java
@@ -0,0 +1,12 @@
+package Reality;
+
+import java.util.ArrayList;
+
+public class Participante {
+	private String nombre;
+	private String apellido;
+	private int edad;
+	private ArrayList<String> generosDePreferencia;
+	private ArrayList<String> idiomasQueCanta;
+	private ArrayList<String> instrumentosQueToca;
+}
